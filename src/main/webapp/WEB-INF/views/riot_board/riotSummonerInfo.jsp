3e9d1ea32e789bfcec616a1daf5619add2fbafb3
Ma <machanghyun0729@gmail.com>
2023-08-24 17:29:53 +0900

    전적검색 완료

--- src/main/webapp/WEB-INF/views/riot_board/riotSummonerInfo.jsp
+++ src/main/webapp/WEB-INF/views/riot_board/riotSummonerInfo.jsp
@@ -20,8 +20,11 @@
 				<button id = 'b1'>검색</button>
 			<!--  검색을 누른 뒤 페이지를 따로 summoners 로 넘어감. -->
 	</div>
+	<div id = "infolist"></div>
 		
 		
+		<h6>button 태그를 form 안에 집어넣기 -> input 엔터로 검색가능</h6>
+		<h6>현재는 console.log 초기화 문제 때문에 밖으로빼놓은 상태</h6>
 		<br><br>
 		
 	<br>
--- src/main/webapp/resources/js/riotInfo.js
+++ src/main/webapp/resources/js/riotInfo.js
@@ -1,4 +1,4 @@
-const api_key = "RGAPI-e072e403-a04e-470c-ab52-086a48a47cd5";
+const api_key = "RGAPI-8c905173-cd12-4063-9c29-d0bc2503e539";
 
 // 함수에서 promise return (resolve)
 /** 버튼 누르면  input value 를 가져온다.
@@ -32,34 +32,108 @@ async function getJson(url) {
       alert("서버문제입니다.");
     }
     let data = await response.json();
-    console.log(response);
+    // console.log(response);
+    // console.log(data);
+    let arr = [
+      data.accountId, // 0 ~ 4 : accountId, id, name, puuid, summonerLevel
+      data.id,
+      data.name,
+      data.puuid,
+      data.summonerLevel,
+    ];
+    // console.log(arr);
+    return arr;
+  } catch (error) {
+    console.log(error);
+    if (
+      error instanceof TypeError &&
+      error.message.includes("Failed to fetch")
+    ) {
+      alert("없는 소환사 이름이거나 API-KEY가 만료되었습니다.");
+    }
+  }
+}
+/** encryptedSummonerId 로 소환사 ID에 대한 리그 정보들을 가져온다.*/
+async function leagueInfo(encID) {
+  let url =
+    "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" +
+    encID +
+    "?api_key=" +
+    api_key;
+  console.log(url);
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+    if (response.status != 200) {
+      alert("서버문제입니다.");
+    }
+
     console.log(data);
+
+    let lP = data[0].leaguePoints;
+    let rank = data[0].rank;
+    let wins = data[0].wins;
+    let losses = data[0].losses;
+    let tier = data[0].tier;
+    console.log(tier, rank, lP, wins, losses);
+    arr = [tier, rank, lP, wins, losses];
+    return arr;
   } catch (error) {
     console.log(error);
   }
-  // console.log(data.id);
-  // console.log(data.name);
-  // console.log(data.puuid);
 }
 
-//  첫 실행
-$(document).ready(async function () {
-  let name = await getSN();
-  let url = await getURL(name);
-  getJson(url);
-  // url 로 fetch 하는거
-});
+/** LeagueInfo 에서 받아온거 jsp 출력 */
+function onJsp(leagueInfo) {
+  // var infolist = document.querySelector("#infolist");
+  $("#infolist").empty();
+  console.log(leagueInfo);
+  var tag = "";
+  let match = leagueInfo[3] + leagueInfo[4];
+  tag +=
+    "<div>" + "솔로랭크 : " + leagueInfo[0] + "\t " + leagueInfo[1] + " </div>";
+  tag += "<div>" + "League Point : " + leagueInfo[2] + "</div>";
+  tag += "<div>" + "승 : " + leagueInfo[3] + " </div>";
+  tag += "<div>" + "패 : " + leagueInfo[4] + " </div>";
+  tag +=
+    "<div>" +
+    "승률 : " +
+    Math.ceil((leagueInfo[3] / match) * 100) +
+    "%" +
+    " </div>";
 
-// 버튼을 눌러도 동작을 안해서 두번째 실행부터 필요한 코드
+  // infolist.innerHTML = tag;
+  $("#infolist").append(tag);
+  // console.log(tag);
+}
+
+/**버튼을 눌러도 동작을 안해서 두번째 실행부터 필요한 코드  + 코드추가 */
 function getOnClick() {
   $(document).ready(async function () {
     let name = await getSN();
     let url = await getURL(name);
-    getJson(url);
+    let InfoResult = await getJson(url);
+    let encId = InfoResult[1]; //encryptedSummonerId
+    let LInfo = await leagueInfo(encId);
+    console.log(LInfo);
+    onJsp(LInfo);
   });
 }
 
-// 버튼을 눌러도 동작을 안해서 두번째 실행부터 필요한 코드
+//  첫 실행  ++ 코드추가 (두번째 실행이랑 코드 맞추기 )
+$(document).ready(async function () {
+  let name = await getSN();
+  let url = await getURL(name);
+  let InfoResult = await getJson(url);
+  let encId = InfoResult[1]; //encryptedSummonerId
+  let LInfo = await leagueInfo(encId);
+  console.log(LInfo);
+  onJsp(LInfo);
+
+  // url 로 fetch 하는거
+});
+
+/**  버튼을 눌러도 동작을 안해서 두번째 실행부터 필요한 코드 + 내용 추가X*/
 $(document).ready(function () {
   $("#b1").click(function (e) {
     getOnClick();
